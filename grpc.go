dbbcfd90cad806c3c38a7788ef30034a5dd4dca9
Sofia Papagiannaki <papagian@users.noreply.github.com>
2019-10-11 17:53:10 +0300

    Add bi-directional support (#3)
    Add callback for enabling a datasource plugin to request
    grafana to execute a datasource query.
    This work was initially introduced in grafana/grafana-plugin-model@117e6583739abaf05f4cd612399b8f10558eaf4c

--- grpc.go
+++ grpc.go
@@ -4,20 +4,78 @@ import (
 	"context"
 
 	"github.com/grafana/grafana-plugin-sdk-go/genproto/datasource"
+	"github.com/hashicorp/go-hclog"
+	"github.com/hashicorp/go-plugin"
+	"google.golang.org/grpc"
 )
 
 type grpcClient struct {
+	broker *plugin.GRPCBroker
 	client datasource.DatasourcePluginClient
 }
 
-func (m *grpcClient) Query(ctx context.Context, req *datasource.DatasourceRequest) (*datasource.DatasourceResponse, error) {
-	return m.client.Query(ctx, req)
+// GrafanaAPI is the Grafana API interface that allows a datasource plugin to callback and request additional information from Grafana.
+type GrafanaAPI interface {
+	QueryDatasource(ctx context.Context, req *datasource.QueryDatasourceRequest) (*datasource.QueryDatasourceResponse, error)
+}
+
+func (m *grpcClient) Query(ctx context.Context, req *datasource.DatasourceRequest, api GrafanaAPI) (*datasource.DatasourceResponse, error) {
+	apiServer := &GRPCGrafanaAPIServer{Impl: api}
+
+	var s *grpc.Server
+	serverFunc := func(opts []grpc.ServerOption) *grpc.Server {
+		s = grpc.NewServer(opts...)
+		datasource.RegisterGrafanaAPIServer(s, apiServer)
+
+		return s
+	}
+	brokeId := m.broker.NextId()
+	go m.broker.AcceptAndServe(brokeId, serverFunc)
+
+	req.RequestId = brokeId
+	res, err := m.client.Query(ctx, req)
+
+	s.Stop()
+	return res, err
 }
 
 type grpcServer struct {
-	Impl datasourcePluginWrapper
+	broker *plugin.GRPCBroker
+	Impl   datasourcePluginWrapper
 }
 
 func (m *grpcServer) Query(ctx context.Context, req *datasource.DatasourceRequest) (*datasource.DatasourceResponse, error) {
-	return m.Impl.Query(ctx, req)
+	conn, err := m.broker.Dial(req.RequestId)
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+
+	api := &GRPCGrafanaAPIClient{datasource.NewGrafanaAPIClient(conn)}
+	return m.Impl.Query(ctx, req, api)
+}
+
+// GRPCGrafanaAPIClient is an implementation of GrafanaAPIClient that talks over RPC.
+type GRPCGrafanaAPIClient struct{ client datasource.GrafanaAPIClient }
+
+func (m *GRPCGrafanaAPIClient) QueryDatasource(ctx context.Context, req *datasource.QueryDatasourceRequest) (*datasource.QueryDatasourceResponse, error) {
+	resp, err := m.client.QueryDatasource(ctx, req)
+	if err != nil {
+		hclog.Default().Info("grafana.QueryDatasource", "client", "start", "err", err)
+		return nil, err
+	}
+	return resp, err
+}
+
+// GRPCGrafanaAPIServer is the gRPC server that GRPCGrafanaAPIClient talks to.
+type GRPCGrafanaAPIServer struct {
+	Impl GrafanaAPI
+}
+
+func (m *GRPCGrafanaAPIServer) QueryDatasource(ctx context.Context, req *datasource.QueryDatasourceRequest) (*datasource.QueryDatasourceResponse, error) {
+	resp, err := m.Impl.QueryDatasource(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
 }
