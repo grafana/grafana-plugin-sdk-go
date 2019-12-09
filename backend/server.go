81ec884c83b9c3df6b3a48799eef76174996dab4
Kyle Brandt <kyle@grafana.com>
2019-12-09 15:03:13 -0500

    s/restHander/restHandler [ci skip]

--- backend/server.go
+++ backend/server.go
@@ -8,14 +8,14 @@ import (
 // Serve starts serving the datasource plugin over gRPC.
 //
 // The plugin ID should be in the format <org>-<name>-datasource.
-func Serve(pluginID string, checkHandler CheckHandler, dataHandler DataQueryHandler, restHander RESTHandler) error {
+func Serve(pluginID string, checkHandler CheckHandler, dataHandler DataQueryHandler, restHandler RESTHandler) error {
 	versionedPlugins := map[int]plugin.PluginSet{
 		common.ProtocolVersion: {
 			pluginID: &DatasourcePluginImpl{
 				Impl: backendPluginWrapper{
 					dataHandler:  dataHandler,
 					checkHandler: checkHandler,
-					restHandler:  restHander,
+					restHandler:  restHandler,
 				},
 			},
 		},
