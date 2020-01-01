98ca30b4418cdc24addd75c37894e891258d87c1
Marcus Efraimsson <marcus.efraimsson@gmail.com>
2020-01-01 12:45:58 +0100

    plugin id not needed in serve

--- backend/server.go
+++ backend/server.go
@@ -6,9 +6,7 @@ import (
 )
 
 // Serve starts serving the datasource plugin over gRPC.
-//
-// The plugin ID should be in the format <org>-<name>-datasource.
-func Serve(pluginID string, backendHandlers PluginHandlers, transformHandlers TransformHandlers) error {
+func Serve(backendHandlers PluginHandlers, transformHandlers TransformHandlers) error {
 	versionedPlugins := make(map[int]plugin.PluginSet)
 
 	pSet := make(plugin.PluginSet)
