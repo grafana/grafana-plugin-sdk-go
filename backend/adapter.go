03a063306f5981278bc991008e0f4f7621a44db9
Marcus Efraimsson <marcus.efraimsson@gmail.com>
2020-01-31 10:12:23 +0100

    Implement support for resources (#51)
    Initial support for resources.
    Refactors and restructures code a bit. 
    
    Ref #34

---
+++ backend/adapter.go
@@ -0,0 +1,102 @@
+package backend
+
+import (
+	"bytes"
+	"context"
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/common/expfmt"
+
+	"github.com/grafana/grafana-plugin-sdk-go/backend/plugin"
+	"github.com/grafana/grafana-plugin-sdk-go/genproto/pluginv2"
+)
+
+// sdkAdapter adapter between low level plugin protocol and SDK interfaces.
+type sdkAdapter struct {
+	CheckHealthHandler   CheckHealthHandler
+	DataQueryHandler     DataQueryHandler
+	CallResourceHandler  CallResourceHandler
+	TransformDataHandler TransformDataHandler
+}
+
+func (a *sdkAdapter) CollectMetrics(ctx context.Context, protoReq *pluginv2.CollectMetrics_Request) (*pluginv2.CollectMetrics_Response, error) {
+	mfs, err := prometheus.DefaultGatherer.Gather()
+	if err != nil {
+		return nil, err
+	}
+
+	var buf bytes.Buffer
+	for _, mf := range mfs {
+		_, err := expfmt.MetricFamilyToText(&buf, mf)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return &pluginv2.CollectMetrics_Response{
+		Metrics: &pluginv2.CollectMetrics_Payload{
+			Prometheus: buf.Bytes(),
+		},
+	}, nil
+}
+
+func (a *sdkAdapter) CheckHealth(ctx context.Context, protoReq *pluginv2.CheckHealth_Request) (*pluginv2.CheckHealth_Response, error) {
+	if a.CheckHealthHandler != nil {
+		res, err := a.CheckHealthHandler.CheckHealth(ctx)
+		if err != nil {
+			return nil, err
+		}
+		return toProto().CheckHealthResponse(res), nil
+	}
+
+	return &pluginv2.CheckHealth_Response{
+		Status: pluginv2.CheckHealth_Response_OK,
+	}, nil
+}
+
+func (a *sdkAdapter) DataQuery(ctx context.Context, req *pluginv2.DataQueryRequest) (*pluginv2.DataQueryResponse, error) {
+	resp, err := a.DataQueryHandler.DataQuery(ctx, fromProto().DataQueryRequest(req))
+	if err != nil {
+		return nil, err
+	}
+
+	return toProto().DataQueryResponse(resp)
+}
+
+func (a *sdkAdapter) CallResource(ctx context.Context, protoReq *pluginv2.CallResource_Request) (*pluginv2.CallResource_Response, error) {
+	if a.CallResourceHandler == nil {
+		return &pluginv2.CallResource_Response{
+			Code: http.StatusNotImplemented,
+		}, nil
+	}
+
+	resp, err := a.CallResourceHandler.CallResource(ctx, fromProto().CallResourceRequest(protoReq))
+	if err != nil {
+		return nil, err
+	}
+
+	return toProto().CallResourceResponse(resp), nil
+}
+
+func (a *sdkAdapter) TransformData(ctx context.Context, req *pluginv2.DataQueryRequest, callBack plugin.TransformCallBack) (*pluginv2.DataQueryResponse, error) {
+	resp, err := a.TransformDataHandler.TransformData(ctx, fromProto().DataQueryRequest(req), &transformCallBackWrapper{callBack})
+	if err != nil {
+		return nil, err
+	}
+
+	return toProto().DataQueryResponse(resp)
+}
+
+type transformCallBackWrapper struct {
+	callBack plugin.TransformCallBack
+}
+
+func (tw *transformCallBackWrapper) DataQuery(ctx context.Context, req *DataQueryRequest) (*DataQueryResponse, error) {
+	protoRes, err := tw.callBack.DataQuery(ctx, toProto().DataQueryRequest(req))
+	if err != nil {
+		return nil, err
+	}
+
+	return fromProto().DataQueryResponse(protoRes)
+}
---
+++ backend/adapter_resource_test.go
@@ -0,0 +1,103 @@
+package backend
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"testing"
+
+	"github.com/grafana/grafana-plugin-sdk-go/genproto/pluginv2"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCallResource(t *testing.T) {
+	t.Run("When call resource handler not set should return http status not implemented", func(t *testing.T) {
+		adapter := &sdkAdapter{}
+		res, err := adapter.CallResource(context.Background(), &pluginv2.CallResource_Request{})
+		require.NoError(t, err)
+		require.NotNil(t, res)
+		require.Equal(t, http.StatusNotImplemented, int(res.Code))
+		require.Empty(t, res.Headers)
+		require.Empty(t, res.Body)
+	})
+
+	t.Run("When call resource handler set should provide expected request and return expected response", func(t *testing.T) {
+		data := map[string]interface{}{
+			"message": "hello",
+		}
+		body, err := json.Marshal(&data)
+		require.NoError(t, err)
+		handler := &testCallResourceHandler{
+			responseStatus: http.StatusOK,
+			responseHeaders: map[string][]string{
+				"X-Header-Out-1": []string{"D", "E"},
+				"X-Header-Out-2": []string{"F"},
+			},
+			responseBody: body,
+		}
+		adapter := &sdkAdapter{
+			CallResourceHandler: handler,
+		}
+		res, err := adapter.CallResource(context.Background(), &pluginv2.CallResource_Request{
+			Config: &pluginv2.PluginConfig{
+				Id:    1,
+				OrgId: 2,
+				Name:  "my-name",
+				Type:  "my-type",
+				Url:   "my-url",
+			},
+			Path:   "some/path",
+			Method: http.MethodGet,
+			Url:    "plugins/test-plugin/resources/some/path?test=1",
+			Headers: map[string]*pluginv2.CallResource_StringList{
+				"X-Header-In-1": &pluginv2.CallResource_StringList{Values: []string{"A", "B"}},
+				"X-Header-In-2": &pluginv2.CallResource_StringList{Values: []string{"C"}},
+			},
+			Body: body,
+		})
+
+		require.NoError(t, err)
+		// request
+		require.NotNil(t, handler.actualReq)
+		require.Equal(t, "some/path", handler.actualReq.Path)
+		require.Equal(t, http.MethodGet, handler.actualReq.Method)
+		require.Equal(t, "plugins/test-plugin/resources/some/path?test=1", handler.actualReq.URL)
+		require.Contains(t, handler.actualReq.Headers, "X-Header-In-1")
+		require.Equal(t, []string{"A", "B"}, handler.actualReq.Headers["X-Header-In-1"])
+		require.Contains(t, handler.actualReq.Headers, "X-Header-In-2")
+		require.Equal(t, []string{"C"}, handler.actualReq.Headers["X-Header-In-2"])
+		var actualRequestData map[string]interface{}
+		err = json.Unmarshal(res.Body, &actualRequestData)
+		require.NoError(t, err)
+		require.Equal(t, data, actualRequestData)
+
+		// response
+		require.NotNil(t, res)
+		require.Equal(t, http.StatusOK, int(res.Code))
+		require.Contains(t, res.Headers, "X-Header-Out-1")
+		require.Equal(t, []string{"D", "E"}, res.Headers["X-Header-Out-1"].Values)
+		require.Contains(t, res.Headers, "X-Header-Out-2")
+		require.Equal(t, []string{"F"}, res.Headers["X-Header-Out-2"].Values)
+		var actualResponseData map[string]interface{}
+		err = json.Unmarshal(res.Body, &actualResponseData)
+		require.NoError(t, err)
+		require.Equal(t, data, actualResponseData)
+	})
+}
+
+type testCallResourceHandler struct {
+	responseStatus  int
+	responseHeaders map[string][]string
+	responseBody    []byte
+	responseErr     error
+	actualReq       *CallResourceRequest
+}
+
+func (h *testCallResourceHandler) CallResource(ctx context.Context, req *CallResourceRequest) (*CallResourceResponse, error) {
+	h.actualReq = req
+	return &CallResourceResponse{
+		Status:  h.responseStatus,
+		Headers: h.responseHeaders,
+		Body:    h.responseBody,
+	}, h.responseErr
+}
