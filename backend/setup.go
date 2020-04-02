796116c704bf5fa7581c9cde6bbe543960ea44a7
Kyle Brandt <kyle@grafana.com>
2020-04-02 11:03:34 -0400

    backend/setup.go: constants and code docs/comments

--- backend/setup.go
+++ backend/setup.go
@@ -7,12 +7,23 @@ import (
 	"os"
 )
 
+const (
+	// PluginProfilerEnv is a constant for the GF_PLUGINS_PROFILER environment variable used to enable pprof.
+	PluginProfilerEnv = "GF_PLUGINS_PROFILER"
+	// PluginProfilerPortEnv is a constant for the GF_PLUGINS_PROFILER_PORT environment variable use to specify a pprof port (default 6060).
+	PluginProfilerPortEnv = "GF_PLUGINS_PROFILER_PORT"
+)
+
 // SetupPluginEnvironment will read the environment variables and apply the
-// standard environment behavior.  As the SDK evolves, this will likely change!
+// standard environment behavior.
+//
+// As the SDK evolves, this will likely change.
+//
+// Currently this function enables and configures profiling with pprof.
 func SetupPluginEnvironment(pluginID string) {
 	// Enable profiler
 	profilerEnabled := false
-	if value, ok := os.LookupEnv("GF_PLUGINS_PROFILER"); ok {
+	if value, ok := os.LookupEnv(PluginProfilerEnv); ok {
 		// compare value to plugin name
 		if value == pluginID {
 			profilerEnabled = true
@@ -21,7 +32,7 @@ func SetupPluginEnvironment(pluginID string) {
 	Logger.Info("Profiler", "enabled", profilerEnabled)
 	if profilerEnabled {
 		profilerPort := "6060"
-		if value, ok := os.LookupEnv("GF_PLUGINS_PROFILER_PORT"); ok {
+		if value, ok := os.LookupEnv(PluginProfilerPortEnv); ok {
 			profilerPort = value
 		}
 
