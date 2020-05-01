8b170eadc7ca6b7165e684bd060542e6bc36fcf8
Kyle Brandt <kyle@grafana.com>
2020-05-01 07:52:19 -0400

    backend: Set refId on Frame if empty (#160)

--- backend/convert_to_protobuf.go
+++ backend/convert_to_protobuf.go
@@ -132,11 +132,18 @@ func (t ConvertToProtobuf) QueryDataRequest(req *QueryDataRequest) *pluginv2.Que
 }
 
 // QueryDataResponse converts the SDK version of a QueryDataResponse to the protobuf version.
+// It will set the RefID on the frames to the RefID key in Responses if a Frame's
+// RefId property is an empty string.
 func (t ConvertToProtobuf) QueryDataResponse(res *QueryDataResponse) (*pluginv2.QueryDataResponse, error) {
 	pQDR := &pluginv2.QueryDataResponse{
 		Responses: make(map[string]*pluginv2.DataResponse, len(res.Responses)),
 	}
 	for refID, dr := range res.Responses {
+		for _, f := range dr.Frames {
+			if f.RefID == "" {
+				f.RefID = refID
+			}
+		}
 		encodedFrames, err := dr.Frames.MarshalArrow()
 		if err != nil {
 			return nil, err
