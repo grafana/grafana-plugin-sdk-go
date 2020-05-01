2de6180746306fc4e5c77cd4af6e5615853c578a
Kyle Brandt <kyle@grafana.com>
2020-05-01 13:57:31 -0400

    data: fix json encoding of notice severity (#163)

---
+++ data/frame_meta_test.go
@@ -0,0 +1,37 @@
+package data
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestJSONNotice(t *testing.T) {
+	tests := []struct {
+		name   string
+		notice Notice
+		json   string
+	}{
+		{
+			name: "notice with severity and text",
+			notice: Notice{
+				Severity: NoticeSeverityError,
+				Text:     "Some text",
+			},
+			json: `{"severity":"error","text":"Some text"}`,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			b, err := json.Marshal(tt.notice)
+			require.NoError(t, err)
+			require.Equal(t, tt.json, string(b))
+
+			n := Notice{}
+			err = json.Unmarshal([]byte(tt.json), &n)
+			require.NoError(t, err)
+			require.Equal(t, tt.notice, n)
+		})
+	}
+}
