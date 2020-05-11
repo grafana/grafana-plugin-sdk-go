ae00561822d3ff0fbc23adc473500876da7c65b4
Ryan McKinley <ryantxu@gmail.com>
2020-05-11 08:14:09 -0700

    Update build/types.go
    Co-authored-by: Arve Knudsen <arve.knudsen@gmail.com>

--- build/types.go
+++ build/types.go
@@ -3,7 +3,7 @@ package build
 // BuildConfig holds the setup variables required for a build
 type BuildConfig struct {
 	OS          string // GOOS
-	ARCH        string // GOOS
+	Arch        string // GOOS
 	EnableDebug bool
 	Env         map[string]string
 }
