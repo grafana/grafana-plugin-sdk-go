5257a2e09676805b8f1ea53a22ecc21b99ef7be9
Sofia Papagiannaki <papagian@users.noreply.github.com>
2020-08-03 17:28:51 +0300

    Add support for enabling cgo (#209)
    * Add support for enabling cgo
    
    * Apply suggestions from code review
    
    Co-authored-by: Arve Knudsen <arve.knudsen@gmail.com>
    
    * gofmt -w build/common.go
    
    Co-authored-by: Arve Knudsen <arve.knudsen@gmail.com>

--- build/common.go
+++ build/common.go
@@ -76,16 +76,28 @@ func buildBackend(cfg Config) error {
 		return err
 	}
 
-	// Link statically
-	ldFlags := `-extldflags "-static"`
+	ldFlags := ""
+	if !cfg.EnableCGo {
+		// Link statically
+		ldFlags = `-extldflags "-static"`
+	}
+
 	if !cfg.EnableDebug {
 		// Add linker flags to drop debug information
-		ldFlags = "-w -s " + ldFlags
+		prefix := ""
+		if ldFlags != "" {
+			prefix = " "
+		}
+		ldFlags = fmt.Sprintf("-w -s%s%s", prefix, ldFlags)
 	}
 
 	args := []string{
-		"build", "-o", path.Join("dist", exeName), "-ldflags", ldFlags,
+		"build", "-o", filepath.Join("dist", exeName),
 	}
+	if ldFlags != "" {
+		args = append(args, "-ldflags", ldFlags)
+	}
+
 	if cfg.EnableDebug {
 		args = append(args, "-gcflags=all=-N -l")
 	}
@@ -93,7 +105,9 @@ func buildBackend(cfg Config) error {
 
 	cfg.Env["GOARCH"] = cfg.Arch
 	cfg.Env["GOOS"] = cfg.OS
-	cfg.Env["CGO_ENABLED"] = "0"
+	if !cfg.EnableCGo {
+		cfg.Env["CGO_ENABLED"] = "0"
+	}
 
 	// TODO: Change to sh.RunWithV once available.
 	return sh.RunWith(cfg.Env, "go", args...)
