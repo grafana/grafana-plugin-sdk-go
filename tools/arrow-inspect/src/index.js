31f63699cbda9632bb0ab67dd769246fb2c8bf77
Ryan McKinley <ryantxu@gmail.com>
2020-08-19 07:47:55 -0700

    remove arrow inspect (#217)

--- tools/arrow-inspect/src/index.js
+++
@@ -1,7 +0,0 @@
-import { readFileSync } from "fs";
-import { Table } from "apache-arrow";
-
-const arrow = readFileSync("../dataframe/dataframe.arrow");
-const table = Table.from([arrow]);
-
-console.log(JSON.stringify(table));
