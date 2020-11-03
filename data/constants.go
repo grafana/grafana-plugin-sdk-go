0df0fe09bab6838be3cca7ad2118de34b4b54611
Torkel Ödegaard <torkel@grafana.com>
2020-11-03 09:42:52 +0100

    Update

--- data/constants.go
+++ data/constants.go
@@ -3,5 +3,5 @@ package data
 // TimeSeriesValueFieldName is the standard name for time series value fields
 const TimeSeriesValueFieldName = "Value"
 
-// TimeTimeSeriesTimeFieldName is the standard name for time series time fields
+// TimeSeriesTimeFieldName is the standard name for time series time fields
 const TimeSeriesTimeFieldName = "Time"
