752ccee774ae277a2cf3c2793ed60b703da55730
Torkel Ödegaard <torkel@grafana.com>
2020-11-03 08:06:04 +0100

    FieldConfig: Added missing data frame field config property displayNameFromDS

--- data/field_config.go
+++ data/field_config.go
@@ -13,8 +13,12 @@ type FieldConfig struct {
 	// https://github.com/grafana/grafana/blob/master/packages/grafana-data/src/types/dataFrame.ts#L23
 	// All properties are optional should be omitted from JSON when empty or not set.
 
+	// This should not be used from a data source
 	DisplayName string `json:"displayName,omitempty"`
-	Filterable  *bool  `json:"filterable,omitempty"` // indicates if the Field's data can be filtered by additional calls.
+	// Use this to control series / field naming from the data datasource
+	DisplayNameFromDS string `json:"displayNameFromDS,omitempty"`
+
+	Filterable *bool `json:"filterable,omitempty"` // indicates if the Field's data can be filtered by additional calls.
 
 	// Numeric Options
 	Unit     string       `json:"unit,omitempty"`     // is the string to display to represent the Field's unit, such as "Requests/sec"
