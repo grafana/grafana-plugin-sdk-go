58a511b76336e9d84eb73e2d81f49291d5405c9e
Torkel Ödegaard <torkel@grafana.com>
2020-11-03 09:34:48 +0100

    Updated const names

--- data/constants.go
+++ data/constants.go
@@ -1,4 +1,4 @@
 package data
 
-const TIME_SERIES_VALUE_FIELD_NAME = "Value"
-const TIME_SERIES_TIME_FIELD_NAME = "Time"
+const TimeSeriesValueFieldName = "Value"
+const TimeSeriesTimeFieldName = "Time"
