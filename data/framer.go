723ca7502a201ee129505db58cde047f2ab5d1b9
Kevin Minehart <kmineh0151@gmail.com>
2020-12-10 10:38:26 -0600

    Update data/framer.go
    Co-authored-by: Kyle Brandt <kyle@grafana.com>

--- data/framer.go
+++ data/framer.go
@@ -2,7 +2,7 @@ package data
 
 // Framer is simply an object that can be converted to Grafana data frames.
 // This interface allows us to interact with types that represent data source objects
-// Without having to convert them to data frames first
+// without having to convert them to data frames first.
 type Framer interface {
 	Frames() (Frames, error)
 }
