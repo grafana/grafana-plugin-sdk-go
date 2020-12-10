e82504ab4b156cfa696b909acb6052535119a38e
Ryan McKinley <ryantxu@gmail.com>
2020-12-09 23:01:36 -0800

    Update data/frame_meta.go
    Co-authored-by: Arve Knudsen <arve.knudsen@gmail.com>

--- data/frame_meta.go
+++ data/frame_meta.go
@@ -13,7 +13,7 @@ type FrameMeta struct {
 	// Path is a browsable path on the datasource
 	Path string `json:"path,omitempty"`
 
-	// PathSeparator defines the separator pattern to decode a heiarchy.  The default separator is '/'
+	// PathSeparator defines the separator pattern to decode a hiearchy. The default separator is '/'
 	PathSeparator string `json:"pathSeparator,omitempty"`
 
 	// Datasource specific values
