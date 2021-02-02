0d4db3896bb9e50446a5d5130fecb87fb8621284
santriseus <santriseus@gmail.com>
2021-02-02 15:41:54 +0300

    Change how InstanceManager manages instance caching (#251)
    This changes how InstanceManager creates, caches and disposes of instances.
    
    Added tests which could clarify the issues reported in bug. Without the changes 
    here this tests should fail. 
    I put the Dispose and code for checking the value existence in the lock section. 
    To optimize it (99.999...% of calls will be just getting the value from cache and 
    returning to the caller after config check) I implemented the double-check locking.
    I have replaced simple sync.RWMutex with implementation of named RWMutex, 
    which allow to process instances with different id in parallel without locking.
    Since I replaced RWMutex with Named RWMutex we starting to have a situation 
    with parallel read/write call to map with cached instances (for example read 
    instance id#1 and write instance id#2), so I had to replace map with sync.Map
    
    Fixes #248
    
    Co-authored-by: Andrei Shamanau <andrei.shamanau@softeq.com>
    Co-authored-by: Marcus Efraimsson <marcus.efraimsson@gmail.com>

--- backend/instancemgmt/instance_manager_test.go
+++ backend/instancemgmt/instance_manager_test.go
@@ -1,6 +1,8 @@
 package instancemgmt
 
 import (
+	"sync"
+	"sync/atomic"
 	"testing"
 	"time"
 
@@ -59,17 +61,155 @@ func TestInstanceManager(t *testing.T) {
 	})
 }
 
+func TestInstanceManagerConcurrency(t *testing.T) {
+	t.Run("Check possible race condition issues when initially creating instance", func(t *testing.T) {
+		tip := &testInstanceProvider{}
+		im := New(tip)
+		ctx := backend.PluginContext{
+			OrgID: 1,
+			AppInstanceSettings: &backend.AppInstanceSettings{
+				Updated: time.Now(),
+			},
+		}
+		var wg sync.WaitGroup
+		wg.Add(10)
+
+		var createdInstances []*testInstance
+		mutex := new(sync.Mutex)
+		// Creating new instances because of updated context
+		for i := 0; i < 10; i++ {
+			go func() {
+				instance, _ := im.Get(ctx)
+				mutex.Lock()
+				defer mutex.Unlock()
+				// Collect all instances created
+				createdInstances = append(createdInstances, instance.(*testInstance))
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+
+		t.Run("All created instances should be either disposed or exist in cache for later disposing", func(t *testing.T) {
+			cachedInstance, _ := im.Get(ctx)
+			for _, instance := range createdInstances {
+				if cachedInstance.(*testInstance) != instance && instance.disposedTimes < 1 {
+					require.FailNow(t, "Found lost reference to un-disposed instance")
+				}
+			}
+		})
+	})
+
+	t.Run("Check possible race condition issues when re-creating instance on settings update", func(t *testing.T) {
+		initialCtx := backend.PluginContext{
+			OrgID: 1,
+			AppInstanceSettings: &backend.AppInstanceSettings{
+				Updated: time.Now(),
+			},
+		}
+		tip := &testInstanceProvider{}
+		im := New(tip)
+		// Creating initial instance with old contexts
+		instanceToDispose, _ := im.Get(initialCtx)
+
+		updatedCtx := backend.PluginContext{
+			OrgID: 1,
+			AppInstanceSettings: &backend.AppInstanceSettings{
+				Updated: time.Now(),
+			},
+		}
+
+		var wg sync.WaitGroup
+		wg.Add(10)
+
+		var createdInstances []*testInstance
+		mutex := new(sync.Mutex)
+		// Creating new instances because of updated context
+		for i := 0; i < 10; i++ {
+			go func() {
+				instance, _ := im.Get(updatedCtx)
+				mutex.Lock()
+				defer mutex.Unlock()
+				// Collect all instances created during concurrent update
+				createdInstances = append(createdInstances, instance.(*testInstance))
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+
+		t.Run("Initial instance should be disposed only once", func(t *testing.T) {
+			require.Equal(t, int64(1), instanceToDispose.(*testInstance).disposedTimes, "Instance should be disposed only once")
+		})
+		t.Run("All created instances should be either disposed or exist in cache for later disposing", func(t *testing.T) {
+			cachedInstance, _ := im.Get(updatedCtx)
+			for _, instance := range createdInstances {
+				if cachedInstance.(*testInstance) != instance && instance.disposedTimes < 1 {
+					require.FailNow(t, "Found lost reference to un-disposed instance")
+				}
+			}
+		})
+	})
+
+	t.Run("Long recreation of instance should not affect datasources with different ID", func(t *testing.T) {
+		const delay = time.Millisecond * 50
+		ctx := backend.PluginContext{
+			OrgID: 1,
+			AppInstanceSettings: &backend.AppInstanceSettings{
+				Updated: time.Now(),
+			},
+		}
+		if testing.Short() {
+			t.Skip("Tests with Sleep")
+		}
+
+		tip := &testInstanceProvider{delay: delay}
+		im := New(tip)
+		// Creating instance with id#1 in cache
+		_, err := im.Get(ctx)
+		require.NoError(t, err)
+		var wg1, wg2 sync.WaitGroup
+		wg1.Add(1)
+		wg2.Add(1)
+		go func() {
+			// Creating instance with id#2 in cache
+			wg1.Done()
+			_, err := im.Get(backend.PluginContext{
+				OrgID: 2,
+				AppInstanceSettings: &backend.AppInstanceSettings{
+					Updated: time.Now(),
+				},
+			})
+			require.NoError(t, err)
+			wg2.Done()
+		}()
+		// Waiting before thread 2 starts to get the instance, so thread 2 could qcquire the lock before thread 1
+		wg1.Wait()
+		// Getting existing instance with id#1 from cache
+		start := time.Now()
+		_, err = im.Get(ctx)
+		elapsed := time.Since(start)
+		require.NoError(t, err)
+		// Waiting before thread 2 finished to get the instance
+		wg2.Wait()
+		if elapsed > delay {
+			require.Fail(t, "Instance should be retrieved from cache without delay")
+		}
+	})
+}
+
 type testInstance struct {
-	orgID    int64
-	updated  time.Time
-	disposed bool
+	orgID         int64
+	updated       time.Time
+	disposed      bool
+	disposedTimes int64
 }
 
 func (ti *testInstance) Dispose() {
 	ti.disposed = true
+	atomic.AddInt64(&ti.disposedTimes, 1)
 }
 
 type testInstanceProvider struct {
+	delay time.Duration
 }
 
 func (tip *testInstanceProvider) GetKey(pluginContext backend.PluginContext) (interface{}, error) {
@@ -83,6 +223,9 @@ func (tip *testInstanceProvider) NeedsUpdate(pluginContext backend.PluginContext
 }
 
 func (tip *testInstanceProvider) NewInstance(pluginContext backend.PluginContext) (Instance, error) {
+	if tip.delay > 0 {
+		time.Sleep(tip.delay)
+	}
 	return &testInstance{
 		orgID:   pluginContext.OrgID,
 		updated: pluginContext.AppInstanceSettings.Updated,
