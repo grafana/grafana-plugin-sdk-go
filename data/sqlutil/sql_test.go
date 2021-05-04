636a1499f3a89420b192a34bbf1a8f9520baeac9
Kevin Minehart <kmineh0151@gmail.com>
2021-05-04 09:25:37 -0500

    remove test

--- data/sqlutil/sql_test.go
+++ data/sqlutil/sql_test.go
@@ -1,17 +1,1 @@
 package sqlutil_test
-
-import (
-	"database/sql"
-	"os"
-	"testing"
-)
-
-func TestMain(m *testing.M) {
-	os.Exit(m.Run())
-}
-
-func TestMakeScanRow(t *testing.T) {
-	t.Run("Should return converters for every column type", func(t *testing.T) {
-		colTypes := make([]*sql.ColumnType, 32)
-	})
-}
