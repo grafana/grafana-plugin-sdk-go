18b90cf8b2cff620ad44c70e902578ee94b525e9
Kevin Minehart <kmineh0151@gmail.com>
2021-05-04 10:21:16 -0500

    Add stringconverter conversion functions

--- data/sqlutil/converter.go
+++ data/sqlutil/converter.go
@@ -19,6 +19,86 @@ type FrameConverter struct {
 	ConverterFunc func(in interface{}) (interface{}, error)
 }
 
+// StringConverter can be used to store types not supported by
+// a Frame into a *string. When scanning, if a SQL's row's InputScanType's Kind
+// and InputScanKind match that returned by the sql response, then the
+// conversion func will be run on the row.
+// Note, a Converter should be favored over a StringConverter as not all SQL rows can be scanned into a string.
+// This type is only here for backwards compatibility.
+type StringConverter struct {
+	// Name is an optional property that can be used to identify a converter
+	Name          string
+	InputScanKind reflect.Kind // reflect.Type might better or worse option?
+	InputTypeName string
+
+	// Conversion func may be nil to do no additional operations on the string conversion.
+	ConversionFunc func(in *string) (*string, error)
+
+	// If the Replacer is not nil, the replacement will be performed.
+	Replacer *StringFieldReplacer
+}
+
+// Note: StringConverter is perhaps better understood as []byte. However, currently
+// the Vector type ([][]byte) is not supported. https://github.com/grafana/grafana-plugin-sdk-go/issues/57
+
+// StringFieldReplacer is used to replace a *string Field in a Frame. The type
+// returned by the ReplaceFunc must match the type of elements of VectorType.
+// Both properties must be non-nil.
+// Note, a Converter should be favored over a StringConverter as not all SQL rows can be scanned into a string.
+// This type is only here for backwards compatibility.
+type StringFieldReplacer struct {
+	OutputFieldType data.FieldType
+	ReplaceFunc     func(in *string) (interface{}, error)
+}
+
+func (s StringConverter) ToConverter() Converter {
+	return Converter{
+		Name:           s.Name,
+		InputScanType:  reflect.TypeOf(""),
+		InputTypeName:  s.InputTypeName,
+		FrameConverter: StringFrameConverter(s),
+	}
+}
+
+// StringFrameConverter creates a FrameConverter from a StringConverter
+func StringFrameConverter(s StringConverter) FrameConverter {
+	f := data.FieldTypeNullableString
+
+	if s.Replacer != nil {
+		f = s.Replacer.OutputFieldType
+	}
+
+	return FrameConverter{
+		FieldType: f,
+		ConverterFunc: func(in interface{}) (interface{}, error) {
+			v := in.(*string)
+			if s.ConversionFunc != nil {
+				converted, err := s.ConversionFunc(v)
+				if err != nil {
+					return nil, err
+				}
+				v = converted
+			}
+
+			if s.Replacer.ReplaceFunc != nil {
+				return s.Replacer.ReplaceFunc(v)
+			}
+
+			return v, nil
+		},
+	}
+}
+
+// ToConverters creates a slice of Converters from a slice of StringConverters
+func ToConverters(s ...StringConverter) []Converter {
+	n := make([]Converter, len(s))
+	for i, v := range s {
+		n[i] = v.ToConverter()
+	}
+
+	return n
+}
+
 // Converter is used to convert known types returned in sql.Row to a type usable in a dataframe.
 type Converter struct {
 	// Name is the name of the converter that is used to distinguish them when debugging or parsing log output
