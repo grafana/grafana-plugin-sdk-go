dbe9e0ddbeeaf47548f2e5e80403b8b666249a83
Alexander Emelin <frvzmb@gmail.com>
2021-05-12 14:44:37 +0300

    use standalone serving for Manage

--- backend/datasource/manage.go
+++ backend/datasource/manage.go
@@ -3,6 +3,7 @@ package datasource
 import (
 	"github.com/grafana/grafana-plugin-sdk-go/backend"
 	"github.com/grafana/grafana-plugin-sdk-go/internal/automanagement"
+	"github.com/grafana/grafana-plugin-sdk-go/internal/standalone"
 )
 
 // ManageOpts can modify Manage behaviour.
@@ -12,13 +13,31 @@ type ManageOpts struct {
 }
 
 // Manage starts serving the data source over gPRC with automatic instance management.
-func Manage(factory InstanceFactoryFunc, opts ManageOpts) error {
+func Manage(id string, factory InstanceFactoryFunc, opts ManageOpts) error {
+	backend.SetupPluginEnvironment(id) // Enable profiler
+
 	handler := automanagement.NewManager(NewInstanceManager(factory))
-	return backend.Serve(backend.ServeOpts{
-		QueryDataHandler:    handler,
+
+	serveOpts := backend.ServeOpts{
 		CheckHealthHandler:  handler,
 		CallResourceHandler: handler,
+		QueryDataHandler:    handler,
 		StreamHandler:       handler,
 		GRPCSettings:        opts.GRPCSettings,
-	})
+	}
+
+	info, err := standalone.GetInfo(id)
+	if err != nil {
+		return err
+	}
+
+	if info.Standalone {
+		return backend.StandaloneServe(serveOpts, info.Address)
+	} else if info.Address != "" {
+		standalone.RunDummyPluginLocator(info.Address)
+		return nil
+	}
+
+	// The default/normal hashicorp path.
+	return backend.Serve(serveOpts)
 }
