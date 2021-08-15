5ed78453d4648459b489f4c1e63ad62f1e3be661
Ryan McKinley <ryantxu@gmail.com>
2021-08-15 11:54:23 -0700

    basic first pass at docs

--- data/frame_type.go
+++ data/frame_type.go
@@ -7,13 +7,32 @@ const (
 	// FrameTypeUnknown indicates that we do not know the field type
 	FrameTypeUnknown FrameType = ""
 
-	// FrameTypeTimeSeriesWide ...
+	// FrameTypeTimeSeriesWide has at least two fields:
+	// field[0]:
+	//  * type time
+	//  * unique ascending values
+	// field[1..n]:
+	//  * distinct labels may be attached to each field
+	//  * numeric & boolean fields can be drawn as lines on a graph
 	FrameTypeTimeSeriesWide = "timeseries-wide"
 
-	// FrameTypeTimeSeriesLong ...
+	// FrameTypeTimeSeriesLong has at least two fields:
+	// field[0]:
+	//  * type time
+	//  * ascending values
+	//  * duplicate times used for different dimensions
+	// field[1..n]:
+	//  * string fields convert to labels
 	FrameTypeTimeSeriesLong = "timeseries-long"
 
-	// FrameTypeTimeSeriesMany ...
+	// FrameTypeTimeSeriesMany has exacty two fields
+	// field[0]:
+	//  * type time
+	//  * ascending values
+	// field[1]:
+	//  * number field
+	//  * labels represent the series dimensions
+	// This structure is typically part of a list of frames with the same structure
 	FrameTypeTimeSeriesMany = "timeseries-many"
 
 	// Soon?
