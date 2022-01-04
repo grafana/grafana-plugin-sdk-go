5f5e3e5577ca9b138f382c22deeea581faff52af
Ryan McKinley <ryantxu@gmail.com>
2022-01-04 17:12:38 -0500

    Use source error as the HealthCheck error message (#389)

--- internal/automanagement/manager.go
+++ internal/automanagement/manager.go
@@ -38,7 +38,10 @@ func (m *Manager) QueryData(ctx context.Context, req *backend.QueryDataRequest) 
 func (m *Manager) CheckHealth(ctx context.Context, req *backend.CheckHealthRequest) (*backend.CheckHealthResult, error) {
 	h, err := m.Get(req.PluginContext)
 	if err != nil {
-		return nil, err
+		return &backend.CheckHealthResult{
+			Status:  backend.HealthStatusError,
+			Message: err.Error(),
+		}, nil
 	}
 	if ds, ok := h.(backend.CheckHealthHandler); ok {
 		return ds.CheckHealth(ctx, req)
