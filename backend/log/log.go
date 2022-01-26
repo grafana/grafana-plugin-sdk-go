4f7bae6affbf6f354a131dd0f5e02f3b5228fa7e
Scott Lepper <scott.lepper@grafana.com>
2022-01-26 08:35:45 -0500

    get log level (#435)
    * get log level
    
    Co-authored-by: Marcus Efraimsson <marcus.efraimsson@gmail.com>

--- backend/log/log.go
+++ backend/log/log.go
@@ -5,20 +5,37 @@ import (
 	hclog "github.com/hashicorp/go-hclog"
 )
 
+type Level int32
+
+const (
+	NoLevel Level = iota
+	Trace
+	Debug
+	Info
+	Warn
+	Error
+)
+
 // Logger is the main Logger interface.
 type Logger interface {
 	Debug(msg string, args ...interface{})
 	Info(msg string, args ...interface{})
 	Warn(msg string, args ...interface{})
 	Error(msg string, args ...interface{})
+	Level() Level
 }
 
 // New creates a new logger.
 func New() Logger {
+	return NewWithLevel(Debug)
+}
+
+// NewWithLevel creates a new logger at the Level defined.
+func NewWithLevel(level Level) Logger {
 	return &hclogWrapper{
 		logger: hclog.New(&hclog.LoggerOptions{
 			// Use debug as level since anything less severe is suppressed.
-			Level: hclog.Debug,
+			Level: hclog.Level(level),
 			// Use JSON format to make the output in Grafana format and work
 			// when using multiple arguments such as Debug("message", "key", "value").
 			JSONFormat: true,
@@ -44,6 +61,25 @@ func (l *hclogWrapper) Warn(msg string, args ...interface{}) {
 
 func (l *hclogWrapper) Error(msg string, args ...interface{}) {
 	l.logger.Error(msg, args...)
+}
+
+func (l *hclogWrapper) Level() Level {
+	if l.logger.IsDebug() {
+		return Debug
+	}
+	if l.logger.IsTrace() {
+		return Trace
+	}
+	if l.logger.IsInfo() {
+		return Info
+	}
+	if l.logger.IsWarn() {
+		return Warn
+	}
+	if l.logger.IsError() {
+		return Error
+	}
+	return NoLevel
 }
 
 // DefaultLogger is the default logger.
