7db62d248f657d2ed24e0b139dd078df803008ce
Ryan McKinley <ryantxu@gmail.com>
2022-01-25 16:12:58 -0800

    add field metadata

---
+++ data/field_meta.go
@@ -0,0 +1,46 @@
+package data
+
+import (
+	"encoding/json"
+)
+
+// FieldMeta attaches structural metadata to each frame
+type FieldMeta struct {
+	// StepSize is the interval between each value.
+	// If no values exist in the values array, this indictes mising data for that interval
+	// Only supports non-zero step sizes
+	StepSize float64 `json:"stepSize,omitempty"`
+
+	// Path is a browsable path on the datasource.
+	StepScale string `json:"stepScale,omitempty"`
+
+	// Aggregation defines the separator pattern to decode a hiearchy. The default separator is '/'.
+	Aggregation string `json:"aggregation,omitempty"`
+
+	// MetricType is 'counter' | 'gauge' | 'summary';
+	MetricType string `json:"metricType,omitempty"`
+}
+
+const (
+	StepScaleLinear = "linear"
+	StepScaleLog2   = "log2"
+	StepScaleLog10  = "log10"
+
+	FieldAggregationBefore = "before"
+	FieldAggregationMiddle = "middle"
+	FieldAggregationAfter  = "after"
+
+	MetricTypeCounter = "counter"
+	MetricTypeGauge   = "gauge"
+	MetricTypeSummary = "summary"
+)
+
+// FrameMetaFromJSON creates a QueryResultMeta from a json string
+func FieldMetaFromJSON(jsonStr string) (*FieldMeta, error) {
+	var m FieldMeta
+	err := json.Unmarshal([]byte(jsonStr), &m)
+	if err != nil {
+		return nil, err
+	}
+	return &m, nil
+}
