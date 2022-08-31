b8d6dd38ff70da9daf7c15cd6d1e7e55e47c994c
gamab <gabi.mabs@gmail.com>
2022-08-31 13:26:52 +0200

    RBAC: Use RBAC as a module

---
+++ backend/accesscontrol/accesscontrol.go
@@ -0,0 +1,93 @@
+package accesscontrol
+
+import (
+	"context"
+	"strings"
+)
+
+// Permissions maps actions to the scopes they can be applied to.
+// ex: { "pluginID.users:read": ["pluginID.users:uid:xHuuebS", "pluginID.users:uid:znbGGd"] }
+type Permissions map[string][]string
+
+// Checker checks whether a user has access to any of the provided scopes.
+type Checker func(scopes ...string) bool
+
+// GenerateChecker generates a function to check whether the user has access to any scope of a given list of scopes.
+func GenerateChecker(ctx context.Context, permissions Permissions, action string, prefixes ...string) Checker {
+	// no permissions => no access to any resource of this type
+	if len(permissions) == 0 {
+		return func(scopes ...string) bool { return false }
+	}
+
+	// no permissions for this action => no access to any resource of this type
+	pScopes, ok := permissions[action]
+	if !ok {
+		return func(scopes ...string) bool { return false }
+	}
+
+	// no prefix expected => only check for the action
+	if len(prefixes) == 0 {
+		return func(scopes ...string) bool { return (len(scopes) == 0 && len(pScopes) == 0) }
+	}
+
+	wildcards := WildcardsFromPrefixes(prefixes...)
+	lookup := make(map[string]bool, len(pScopes)-1)
+	for _, s := range pScopes {
+		// one scope is a wildcard => access to all resources of this type
+		if wildcards.Contains(s) {
+			return func(scopes ...string) bool { return true }
+		}
+		lookup[s] = true
+	}
+
+	return func(scopes ...string) bool {
+		// search for any direct match
+		for _, s := range scopes {
+			if lookup[s] {
+				return true
+			}
+		}
+		return false
+	}
+}
+
+// WildcardsFromPrefixes generates valid wildcards from prefixes
+// pluginID.users:uid: => "*", "pluginID.users:uid::*", "pluginID.users:uid:uid:*"
+func WildcardsFromPrefixes(prefixes ...string) Wildcards {
+	if len(prefixes) == 0 {
+		return Wildcards{}
+	}
+
+	wildcards := Wildcards{"*"}
+	alreadyAdded := map[string]bool{}
+	for _, prefix := range prefixes {
+		var b strings.Builder
+		parts := strings.Split(prefix, ":")
+		for _, p := range parts {
+			if p == "" {
+				continue
+			}
+			b.WriteString(p)
+			b.WriteRune(':')
+			wildcard := b.String() + "*"
+			if !alreadyAdded[wildcard] {
+				wildcards = append(wildcards, b.String()+"*")
+			}
+		}
+	}
+	return wildcards
+}
+
+// Wildcards is an helper to see if a scope is contained within it.
+// ex: "pluginID.users:uid:*" is included in the list of following wildcards ["*", "pluginID.users:*", "pluginID.users:uid:*"]
+type Wildcards []string
+
+// Contains check if wildcards contains scope
+func (wildcards Wildcards) Contains(scope string) bool {
+	for _, w := range wildcards {
+		if scope == w {
+			return true
+		}
+	}
+	return false
+}
---
+++ backend/accesscontrol/accesscontrol_test.go
@@ -0,0 +1,188 @@
+package accesscontrol
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_WildcardsFromPrefixes(t *testing.T) {
+	tests := []struct {
+		name     string
+		prefixes []string
+		want     Wildcards
+	}{
+		{
+			name:     "no prefix",
+			prefixes: []string{},
+			want:     []string{},
+		},
+		{
+			name:     "empty prefix",
+			prefixes: []string{""},
+			want:     []string{"*"},
+		},
+		{
+			name:     "one prefix",
+			prefixes: []string{"plugin_id.a:uid:"},
+			want:     []string{"*", "plugin_id.a:*", "plugin_id.a:uid:*"},
+		},
+		{
+			name:     "two prefixes",
+			prefixes: []string{"plugin_id.a:uid:", "plugin_id.b:uid:"},
+			want:     []string{"*", "plugin_id.a:*", "plugin_id.a:uid:*", "plugin_id.b:*", "plugin_id.b:uid:*"},
+		},
+		{
+			name:     "long prefix",
+			prefixes: []string{"plugin_id.a:sub:uid:"},
+			want:     []string{"*", "plugin_id.a:*", "plugin_id.a:sub:*", "plugin_id.a:sub:uid:*"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, WildcardsFromPrefixes(tt.prefixes...), tt.want)
+		})
+	}
+}
+
+func TestGenerateChecker(t *testing.T) {
+	userPermissions := Permissions{
+		"pid.dashboards:create": []string{},                                                                                         // no scope
+		"pid.dashboards:read":   []string{"pid.dashboards:uid:*", "pid.folders:uid:*"},                                              // wildcards
+		"pid.dashboards:write":  []string{"pid.dashboards:uid:1", "pid.dashboards:uid:2", "pid.folders:uid:3", "pid.folders:uid:4"}, // folders or dashboards
+		"pid.dashboards:delete": []string{"pid.folders:uid:3"},                                                                      // should have no scope
+	}
+
+	type match struct {
+		scopes    []string
+		hasAccess bool
+	}
+	tests := []struct {
+		name        string
+		permissions Permissions
+		action      string
+		prefixes    []string
+		want        match
+	}{
+		{
+			name:        "no match user has no permission",
+			permissions: Permissions{},
+			action:      "pid.dashboards:read",
+			prefixes:    []string{"pid.dashboards:uid"},
+			want:        match{scopes: []string{"pid.dashboards:uid:1"}, hasAccess: false},
+		},
+		{
+			name:        "no match user does not have the permission",
+			permissions: userPermissions,
+			action:      "pid.folders:read",
+			prefixes:    []string{"pid.folders:uid"},
+			want:        match{scopes: []string{"pid.folders:uid:2"}, hasAccess: false},
+		},
+		{
+			name:        "match on action only",
+			permissions: userPermissions,
+			action:      "pid.dashboards:create",
+			prefixes:    []string{},
+			want:        match{scopes: []string{}, hasAccess: true},
+		},
+		{
+			name:        "no match on action only",
+			permissions: userPermissions,
+			action:      "pid.dashboards:print",
+			prefixes:    []string{},
+			want:        match{scopes: []string{}, hasAccess: false},
+		},
+		{
+			name:        "no match on action only when user action is scoped",
+			permissions: userPermissions,
+			action:      "pid.dashboards:delete",
+			prefixes:    []string{},
+			want:        match{scopes: []string{}, hasAccess: false},
+		},
+		{
+			name:        "match user has specific permission",
+			permissions: userPermissions,
+			action:      "pid.dashboards:write",
+			prefixes:    []string{"pid.dashboards:uid"},
+			want:        match{scopes: []string{"pid.dashboards:uid:2"}, hasAccess: true},
+		},
+		{
+			name:        "match user has wildcard permission",
+			permissions: userPermissions,
+			action:      "pid.dashboards:read",
+			prefixes:    []string{"pid.dashboards:uid"},
+			want:        match{scopes: []string{"pid.dashboards:uid:1"}, hasAccess: true},
+		},
+		{
+			name:        "no match user has action but on none of the desired",
+			permissions: userPermissions,
+			action:      "pid.dashboards:write",
+			prefixes:    []string{"pid.dashboards:uid"},
+			want:        match{scopes: []string{"pid.dashboards:uid:55", "pid.dashboards:uid:56"}, hasAccess: false},
+		},
+		{
+			name:        "match checker built with multiple prefixes",
+			permissions: userPermissions,
+			action:      "pid.dashboards:write",
+			prefixes:    []string{"pid.dashboards:uid", "pid.folders:uid"},
+			want:        match{scopes: []string{"pid.dashboard:uid:55", "pid.folders:uid:3"}, hasAccess: true},
+		},
+		{
+			name:        "match when at least one scope is found",
+			permissions: userPermissions,
+			action:      "pid.dashboards:write",
+			prefixes:    []string{"pid.dashboards:uid"},
+			want:        match{scopes: []string{"pid.dashboards:uid:55", "pid.dashboards:uid:2"}, hasAccess: true},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			checker := GenerateChecker(context.Background(), tt.permissions, tt.action, tt.prefixes...)
+			got := checker(tt.want.scopes...)
+			require.Equal(t, tt.want.hasAccess, got)
+		})
+	}
+}
+
+func TestCheckerExamples(t *testing.T) {
+	type dashboard struct {
+		UID       string
+		parentUID string
+	}
+
+	userPermissions := Permissions{
+		"pid.dashboards:create": []string{},
+		"pid.dashboards:read":   []string{"pid.dashboards:uid:dash1", "pid.dashboards:uid:dash2", "pid.folders:uid:fold1", "pid.folders:uid:fold2"},
+	}
+
+	dashboards := []dashboard{
+		{UID: "dash1", parentUID: "fold1"}, // Can read dash directly and through folder
+		{UID: "dash2", parentUID: "fold3"}, // Can read dash directly
+		{UID: "dash3", parentUID: "fold2"}, // Can read dash through folder
+		{UID: "dash4", parentUID: "fold3"}, // Cannot read dash
+	}
+
+	// Check on action only
+	canCreateDashboards := GenerateChecker(context.Background(), userPermissions, "pid.dashboards:create")
+	require.True(t, canCreateDashboards())
+	canDeleteDashboards := GenerateChecker(context.Background(), userPermissions, "pid.dashboards:delete")
+	require.False(t, canDeleteDashboards())
+
+	// Check on either dashboard or folder
+	canReadDashboards := GenerateChecker(context.Background(), userPermissions, "pid.dashboards:read", "pid.folders:read")
+	require.True(t, canReadDashboards("pid.dashboards:uid:dash2"), "should be allowed to read dashboard")
+	require.True(t, canReadDashboards("pid.folders:uid:fold2"), "should be allowed to read dashboard in the folder")
+	require.True(t, canReadDashboards("pid.dashboards:uid:dash3", "pid.folders:uid:fold2"), "should be allowed to read dashboards in the folder")
+	require.False(t, canReadDashboards("pid.dashboards:uid:dash4", "pid.folders:uid:fold3"), "should not be allowed to read dashboards (directly or through folder)")
+
+	// Filter resources
+	readOkUIDs := []string{}
+	for _, dash := range dashboards {
+		if canReadDashboards("pid.dashboards:uid:"+dash.UID, "pid.folders:uid:"+dash.parentUID) {
+			readOkUIDs = append(readOkUIDs, dash.UID)
+		}
+	}
+	require.EqualValues(t, []string{"dash1", "dash2", "dash3"}, readOkUIDs)
+}
