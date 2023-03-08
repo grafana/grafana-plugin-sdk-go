cdc5808e01ce7d8c5db8665207b0035bbb1fd82c
Giuseppe Guerra <giuseppe.guerra@grafana.com>
2023-03-08 17:13:19 +0100

    Backend plugin debug mode improvements (#621)
    * Add support for debug mode when running from GoLand
    
    * Add support for -debug flag to explicitly run the plugin in debug mode
    
    * Fix plugin killing itself when running with -debug flag
    
    * Update comments as per PR review
    
    * Update comment
    
    * Improve debug mode plugin management
    
    - Store PID in standalone file, to allow Grafana to detect if a debug
      plugin is running or not. This allows Grafana to start the plugin with
      the default Hashicorp mode if the debug plugin isn't running anymore.
    - Graceful shutdown of GRPC server, with standalone file cleanup
    
    Note: needs to be tested on Mac OS X and Windows
    
    * Fix comment indentation
    
    * Split standalone address and pid in two different files
    Fixes the dummy locator server not reading the address properly if it's been compiled with the old version SDK, but the standalone.txt comes from the new version of the SDK
    
    * Fix standalone.txt not being cleaned
    
    * Kill dummy plugin locator when stopping debug plugin
    
    * Ensure plugin is running before starting dummy plugin locator
    
    * Fix GoLand debug mode when using non-standard config name, add some tests
    
    * Fix goimports

--- backend/datasource/manage.go
+++ backend/datasource/manage.go
@@ -3,7 +3,6 @@ package datasource
 import (
 	"github.com/grafana/grafana-plugin-sdk-go/backend"
 	"github.com/grafana/grafana-plugin-sdk-go/internal/automanagement"
-	"github.com/grafana/grafana-plugin-sdk-go/internal/standalone"
 )
 
 // ManageOpts can modify Manage behaviour.
@@ -16,29 +15,12 @@ type ManageOpts struct {
 // pluginID should match the one from plugin.json.
 func Manage(pluginID string, instanceFactory InstanceFactoryFunc, opts ManageOpts) error {
 	backend.SetupPluginEnvironment(pluginID) // Enable profiler.
-
 	handler := automanagement.NewManager(NewInstanceManager(instanceFactory))
-
-	serveOpts := backend.ServeOpts{
+	return backend.Manage(pluginID, backend.ServeOpts{
 		CheckHealthHandler:  handler,
 		CallResourceHandler: handler,
 		QueryDataHandler:    handler,
 		StreamHandler:       handler,
 		GRPCSettings:        opts.GRPCSettings,
-	}
-
-	info, err := standalone.GetInfo(pluginID)
-	if err != nil {
-		return err
-	}
-
-	if info.Standalone {
-		return backend.StandaloneServe(serveOpts, info.Address)
-	} else if info.Address != "" {
-		standalone.RunDummyPluginLocator(info.Address)
-		return nil
-	}
-
-	// The default/normal hashicorp path.
-	return backend.Serve(serveOpts)
+	})
 }
