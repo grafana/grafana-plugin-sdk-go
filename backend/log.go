51d836beb3e607b78c7a33916abbc2a3d1f6eea6
Andres Martinez Gotor <andres.martinez@grafana.com>
2023-07-17 12:54:21 +0200

    Add backend.NewLoggerWith function (#715)

--- backend/log.go
+++ backend/log.go
@@ -7,3 +7,8 @@ import (
 // Logger is the default logger instance. This can be used directly to log from
 // your plugin to grafana-server with calls like backend.Logger.Debug(...).
 var Logger = log.DefaultLogger
+
+// NewLoggerWith creates a new logger with the given arguments.
+var NewLoggerWith = func(args ...interface{}) log.Logger {
+	return log.New().With(args...)
+}
