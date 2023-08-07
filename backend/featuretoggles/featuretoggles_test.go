29527397eba16d86523cc638f17f759da1a57c27
Giuseppe Guerra <giuseppe@guerra.in>
2023-08-07 12:47:23 +0200

    Fix tests

--- backend/featuretoggles/featuretoggles_test.go
+++ backend/featuretoggles/featuretoggles_test.go
@@ -9,20 +9,20 @@ import (
 func TestEnvFeatureToggles(t *testing.T) {
 	t.Run("empty", func(t *testing.T) {
 		t.Setenv(envFeatureTogglesEnable, "")
-		flags := New()
+		flags := newFeatureTogglesFromEnv()
 		require.False(t, flags.IsEnabled("abc"))
 	})
 
 	t.Run("single", func(t *testing.T) {
 		t.Setenv(envFeatureTogglesEnable, "abc")
-		flags := New()
+		flags := newFeatureTogglesFromEnv()
 		require.True(t, flags.IsEnabled("abc"))
 		require.False(t, flags.IsEnabled("def"))
 	})
 
 	t.Run("multiple", func(t *testing.T) {
 		t.Setenv(envFeatureTogglesEnable, "abc def")
-		flags := New()
+		flags := newFeatureTogglesFromEnv()
 		require.True(t, flags.IsEnabled("abc"))
 		require.True(t, flags.IsEnabled("def"))
 		require.False(t, flags.IsEnabled("ghi"))
