2028450dd9ffbe785f9ed53fe16c7a21a64c0f9c
Will Browne <will.browne@grafana.com>
2023-09-20 16:15:54 +0100

    add tests for useragent.New

--- backend/useragent/user_agent_test.go
+++ backend/useragent/user_agent_test.go
@@ -6,7 +6,51 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestFromString(t *testing.T) {
+func TestNew(t *testing.T) {
+	type args struct {
+		grafanaVersion string
+		os             string
+		arch           string
+	}
+	tcs := []struct {
+		name string
+		args args
+		want *UserAgent
+		err  error
+	}{
+		{
+			name: "valid",
+			args: args{
+				grafanaVersion: "10.2.0",
+				os:             "darwin",
+				arch:           "amd64",
+			},
+			want: &UserAgent{
+				grafanaVersion: "10.2.0",
+				os:             "darwin",
+				arch:           "amd64",
+			},
+		},
+		{
+			name: "invalid (not semver)",
+			args: args{
+				grafanaVersion: "10.2",
+				os:             "darwin",
+				arch:           "amd64",
+			},
+			err: errInvalidFormat,
+		},
+	}
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			got, err := New(tc.args.grafanaVersion, tc.args.os, tc.args.arch)
+			require.ErrorIs(t, err, tc.err)
+			require.Equal(t, tc.want, got, "New(%v, %v, %v)", tc.args.grafanaVersion, tc.args.os, tc.args.arch)
+		})
+	}
+}
+
+func TestParse(t *testing.T) {
 	tcs := []struct {
 		name      string
 		userAgent string
