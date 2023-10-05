3fcb9f2bfba964f5e7c9c2696eaf4f5fd94f6448
scottlepp <scott.lepper@gmail.com>
2023-10-05 14:04:16 -0400

    add plugin name label

--- experimental/errorsource/error_source_middleware.go
+++ experimental/errorsource/error_source_middleware.go
@@ -17,17 +17,17 @@ var (
 		Namespace: "plugins",
 		Name:      "datasource_error_total",
 		Help:      "Total number of times a plugin errored",
-	}, []string{"error_source"})
+	}, []string{"plugin_name", "error_source"})
 )
 
 // ErrorSourceMiddleware captures error source metric
-func ErrorSourcenMiddleware() httpclient.Middleware {
-	return httpclient.NamedMiddlewareFunc(ErrorSourceMiddlewareName, func(opts httpclient.Options, next http.RoundTripper) http.RoundTripper {
+func ErrorSourcenMiddleware(plugin string) httpclient.Middleware {
+	return httpclient.NamedMiddlewareFunc(plugin, func(opts httpclient.Options, next http.RoundTripper) http.RoundTripper {
 		return httpclient.RoundTripperFunc(func(req *http.Request) (*http.Response, error) {
 			res, err := next.RoundTrip(req)
 			if res != nil && res.StatusCode >= 400 {
 				errorSource := backend.GetErrorSource(res.StatusCode)
-				errors.WithLabelValues(string(errorSource)).Inc()
+				errors.WithLabelValues(plugin, string(errorSource)).Inc()
 			}
 			return res, err
 		})
