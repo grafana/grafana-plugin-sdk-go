21dbe4be6bdc0a43a529669cae4db71158eea438
scottlepp <scott.lepper@gmail.com>
2023-10-05 12:47:15 -0400

    middleware to capture error source metric

---
+++ experimental/errorsource/error_source_middleware.go
@@ -0,0 +1,35 @@
+package errorsource
+
+import (
+	"net/http"
+
+	"github.com/grafana/grafana-plugin-sdk-go/backend"
+	"github.com/grafana/grafana-plugin-sdk-go/backend/httpclient"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+// ErrorSourceMiddlewareName is the middleware name used by ErrorSourceMiddleware.
+const ErrorSourceMiddlewareName = "ErrorSource"
+
+var (
+	errors = promauto.NewCounterVec(prometheus.CounterOpts{
+		Namespace: "plugins",
+		Name:      "datasource_error_total",
+		Help:      "Total number of times a plugin errored",
+	}, []string{"error_source"})
+)
+
+// ErrorSourceMiddleware captures error source metric
+func ErrorSourcenMiddleware() httpclient.Middleware {
+	return httpclient.NamedMiddlewareFunc(ErrorSourceMiddlewareName, func(opts httpclient.Options, next http.RoundTripper) http.RoundTripper {
+		return httpclient.RoundTripperFunc(func(req *http.Request) (*http.Response, error) {
+			res, err := next.RoundTrip(req)
+			if res != nil && res.StatusCode >= 400 {
+				errorSource := backend.GetErrorSource(res.StatusCode)
+				errors.WithLabelValues(string(errorSource)).Inc()
+			}
+			return res, err
+		})
+	})
+}
