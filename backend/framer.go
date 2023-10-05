0b726e54b92f5d9f0c9512478fd5bd029e1c1592
scottlepp <scott.lepper@gmail.com>
2023-10-05 09:37:39 -0400

    revert

--- backend/framer.go
+++ backend/framer.go
@@ -1,8 +1,6 @@
 package backend
 
-import (
-	"github.com/grafana/grafana-plugin-sdk-go/data"
-)
+import "github.com/grafana/grafana-plugin-sdk-go/data"
 
 // FrameResponse creates a DataResponse that contains the Framer's data.Frames.
 func FrameResponse(f data.Framer) *DataResponse {
