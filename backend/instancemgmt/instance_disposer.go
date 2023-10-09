12f93fb01ba11e5602fe899958ddff55ec82ad0a
Will Browne <will.browne@grafana.com>
2023-10-09 15:22:58 +0100

    separate file

---
+++ backend/instancemgmt/instance_disposer.go
@@ -0,0 +1,56 @@
+package instancemgmt
+
+import "sync"
+
+// instanceDisposer tracks and disposes of disposable instances.
+type instanceDisposer struct {
+	cache sync.Map
+	m     sync.RWMutex
+}
+
+// newInstanceDisposer creates a new instanceDisposer.
+func newInstanceDisposer() instanceDisposer {
+	return instanceDisposer{
+		cache: sync.Map{},
+	}
+}
+
+// disposable returns whether an instance is disposable.
+func (d *instanceDisposer) disposable(i Instance) (InstanceDisposer, bool) {
+	if id, ok := i.(InstanceDisposer); ok {
+		return id, true
+	}
+	return nil, false
+}
+
+// track tracks a disposable instance. If there is a disposable instance already, it will be disposed of first.
+func (d *instanceDisposer) track(cacheKey interface{}, id InstanceDisposer) {
+	d.m.Lock()
+	defer d.m.Unlock()
+
+	// If there is a disposable instance already, we should dispose of it first.
+	if i, exists := d.cache.LoadAndDelete(cacheKey); exists {
+		i.(InstanceDisposer).Dispose()
+		activeInstances.Dec()
+	}
+
+	d.cache.Store(cacheKey, id)
+}
+
+// tracking returns whether a disposable instance is being tracked.
+func (d *instanceDisposer) tracking(cacheKey interface{}) bool {
+	d.m.RLock()
+	defer d.m.RUnlock()
+	_, exists := d.cache.Load(cacheKey)
+	return exists
+}
+
+// dispose disposes of a disposable instance.
+func (d *instanceDisposer) dispose(cacheKey interface{}) {
+	d.m.Lock()
+	defer d.m.Unlock()
+	if i, ok := d.cache.LoadAndDelete(cacheKey); ok {
+		i.(InstanceDisposer).Dispose()
+		activeInstances.Dec()
+	}
+}
