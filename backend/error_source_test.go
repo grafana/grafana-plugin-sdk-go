5ba7f7f928aaa06912e6c573c72d3bad18c15de7
scottlepp <scott.lepper@gmail.com>
2023-10-26 16:27:32 -0400

    use custom error to capture source

--- backend/error_source_test.go
+++ backend/error_source_test.go
@@ -1,42 +1,40 @@
-package errorsource
+package backend
 
 import (
 	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/require"
-
-	"github.com/grafana/grafana-plugin-sdk-go/backend"
 )
 
 func TestResponse(t *testing.T) {
 	for _, tc := range []struct {
 		name            string
 		err             error
-		expStatus       backend.Status
+		expStatus       Status
 		expErrorMessage string
-		expErrorSource  backend.ErrorSource
+		expErrorSource  ErrorSource
 	}{
 		{
 			name:            "generic error",
 			err:             errors.New("other"),
-			expStatus:       backend.StatusUnknown,
+			expStatus:       StatusUnknown,
 			expErrorMessage: "other",
-			expErrorSource:  backend.ErrorSourcePlugin,
+			expErrorSource:  ErrorSourcePlugin,
 		},
 		{
 			name:            "downstream error",
 			err:             DownstreamError(errors.New("bad gateway"), false),
 			expStatus:       0,
 			expErrorMessage: "bad gateway",
-			expErrorSource:  backend.ErrorSourceDownstream,
+			expErrorSource:  ErrorSourceDownstream,
 		},
 		{
 			name:            "plugin error",
 			err:             PluginError(errors.New("internal error"), false),
 			expStatus:       0,
 			expErrorMessage: "internal error",
-			expErrorSource:  backend.ErrorSourcePlugin,
+			expErrorSource:  ErrorSourcePlugin,
 		},
 	} {
 		t.Run(tc.name, func(t *testing.T) {
@@ -50,29 +48,29 @@ func TestResponse(t *testing.T) {
 }
 
 func TestResponseWithOptions(t *testing.T) {
-	unknown := New(errors.New("unknown"), backend.ErrorSourcePlugin, backend.StatusUnknown)
-	badgateway := New(errors.New("bad gateway"), backend.ErrorSourceDownstream, backend.StatusBadGateway)
+	unknown := NewError(errors.New("unknown"), ErrorSourcePlugin, StatusUnknown)
+	badgateway := NewError(errors.New("bad gateway"), ErrorSourceDownstream, StatusBadGateway)
 
 	for _, tc := range []struct {
 		name            string
 		err             Error
-		expStatus       backend.Status
+		expStatus       Status
 		expErrorMessage string
-		expErrorSource  backend.ErrorSource
+		expErrorSource  ErrorSource
 	}{
 		{
 			name:            "unknown error",
 			err:             unknown,
-			expStatus:       backend.StatusUnknown,
+			expStatus:       StatusUnknown,
 			expErrorMessage: unknown.Error(),
-			expErrorSource:  backend.ErrorSourcePlugin,
+			expErrorSource:  ErrorSourcePlugin,
 		},
 		{
 			name:            "bad gateway",
 			err:             badgateway,
-			expStatus:       backend.StatusBadGateway,
+			expStatus:       StatusBadGateway,
 			expErrorMessage: badgateway.Error(),
-			expErrorSource:  backend.ErrorSourceDownstream,
+			expErrorSource:  ErrorSourceDownstream,
 		},
 	} {
 		t.Run(tc.name, func(t *testing.T) {
