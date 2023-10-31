bb8a31f376f22d204677ae3bfba15bd9e953e06d
scottlepp <scott.lepper@gmail.com>
2023-10-31 14:54:01 -0400

    externalize the roundtripper

--- experimental/errorsource/error_source_middleware.go
+++ experimental/errorsource/error_source_middleware.go
@@ -11,20 +11,23 @@ import (
 
 // Middleware captures error source metric
 func Middleware(plugin string) httpclient.Middleware {
-	return httpclient.NamedMiddlewareFunc(plugin, func(opts httpclient.Options, next http.RoundTripper) http.RoundTripper {
-		return httpclient.RoundTripperFunc(func(req *http.Request) (*http.Response, error) {
-			res, err := next.RoundTrip(req)
-			if res != nil && res.StatusCode >= 400 {
-				errorSource := backend.ErrorSourceFromHTTPStatus(res.StatusCode)
-				if err == nil {
-					err = errors.New(res.Status)
-				}
-				return nil, Error{source: errorSource, err: err}
+	return httpclient.NamedMiddlewareFunc(plugin, RoundTripper)
+}
+
+// RoundTripper returns the error source
+func RoundTripper(_ httpclient.Options, next http.RoundTripper) http.RoundTripper {
+	return httpclient.RoundTripperFunc(func(req *http.Request) (*http.Response, error) {
+		res, err := next.RoundTrip(req)
+		if res != nil && res.StatusCode >= 400 {
+			errorSource := backend.ErrorSourceFromHTTPStatus(res.StatusCode)
+			if err == nil {
+				err = errors.New(res.Status)
 			}
-			if errors.Is(err, syscall.ECONNREFUSED) {
-				return nil, Error{source: backend.ErrorSourceDownstream, err: err}
-			}
-			return res, err
-		})
+			return nil, Error{source: errorSource, err: err}
+		}
+		if errors.Is(err, syscall.ECONNREFUSED) {
+			return nil, Error{source: backend.ErrorSourceDownstream, err: err}
+		}
+		return res, err
 	})
 }
