fa6fb2a31eb07214461afd087766affbac3e0922
scottlepp <scott.lepper@gmail.com>
2023-10-31 09:41:16 -0400

    don't need a unique name

--- experimental/httpclient/http_client.go
+++ experimental/httpclient/http_client.go
@@ -3,15 +3,15 @@ package httpclient
 import (
 	"net/http"
 
-	"github.com/google/uuid"
 	"github.com/grafana/grafana-plugin-sdk-go/backend/httpclient"
 	"github.com/grafana/grafana-plugin-sdk-go/experimental/errorsource"
 )
 
+const name = "errorsource"
+
 // New wraps the existing http client constructor and adds the error source middleware
 func New(opts *httpclient.Options) (*http.Client, error) {
-	id := uuid.New()
-	opts.Middlewares = append(opts.Middlewares, errorsource.Middleware(id.String()))
+	opts.Middlewares = append(opts.Middlewares, errorsource.Middleware(name))
 	c, err := httpclient.New(*opts)
 	if err != nil {
 		return nil, err
