8a4766c52a793fe1785c18d51a917daad2c60d98
ismail simsek <ismailsimsek09@gmail.com>
2024-03-07 16:34:17 +0100

    linting

--- data/utils/maputil/maputil.go
+++ data/utils/maputil/maputil.go
@@ -10,10 +10,10 @@ func GetMap(obj map[string]any, key string) (map[string]any, error) {
 			err := fmt.Errorf("the field '%s' should be an object", key)
 			return nil, err
 		}
-	} else {
-		err := fmt.Errorf("the field '%s' should be set", key)
-		return nil, err
 	}
+
+	err := fmt.Errorf("the field '%s' should be set", key)
+	return nil, err
 }
 
 func GetMapOptional(obj map[string]any, key string) (map[string]any, error) {
@@ -24,10 +24,10 @@ func GetMapOptional(obj map[string]any, key string) (map[string]any, error) {
 			err := fmt.Errorf("the field '%s' should be an object", key)
 			return nil, err
 		}
-	} else {
-		// Value optional, not error
-		return nil, nil
 	}
+
+	// Value optional, not error
+	return nil, nil
 }
 
 func GetBool(obj map[string]any, key string) (bool, error) {
@@ -38,10 +38,10 @@ func GetBool(obj map[string]any, key string) (bool, error) {
 			err := fmt.Errorf("the field '%s' should be a bool", key)
 			return false, err
 		}
-	} else {
-		err := fmt.Errorf("the field '%s' should be set", key)
-		return false, err
 	}
+
+	err := fmt.Errorf("the field '%s' should be set", key)
+	return false, err
 }
 
 func GetBoolOptional(obj map[string]any, key string) (bool, error) {
@@ -52,10 +52,10 @@ func GetBoolOptional(obj map[string]any, key string) (bool, error) {
 			err := fmt.Errorf("the field '%s' should be a bool", key)
 			return false, err
 		}
-	} else {
-		// Value optional, not error
-		return false, nil
 	}
+
+	// Value optional, not error
+	return false, nil
 }
 
 func GetString(obj map[string]any, key string) (string, error) {
@@ -66,10 +66,10 @@ func GetString(obj map[string]any, key string) (string, error) {
 			err := fmt.Errorf("the field '%s' should be a string", key)
 			return "", err
 		}
-	} else {
-		err := fmt.Errorf("the field '%s' should be set", key)
-		return "", err
 	}
+
+	err := fmt.Errorf("the field '%s' should be set", key)
+	return "", err
 }
 
 func GetStringOptional(obj map[string]any, key string) (string, error) {
@@ -80,8 +80,8 @@ func GetStringOptional(obj map[string]any, key string) (string, error) {
 			err := fmt.Errorf("the field '%s' should be a string", key)
 			return "", err
 		}
-	} else {
-		// Value optional, not error
-		return "", nil
 	}
+
+	// Value optional, not error
+	return "", nil
 }
