cc20697ea045e0e1dfb3fcb1da3f816fc748a263
Alex Weaver <weaver.alex.d@gmail.com>
2024-06-20 11:17:15 -0500

    Handle kube-style error responses

--- experimental/apis/data/v0alpha1/client_test.go
+++ experimental/apis/data/v0alpha1/client_test.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/grafana/grafana-plugin-sdk-go/experimental/apis/data/v0alpha1"
@@ -18,7 +19,45 @@ func TestClientWithBadURL(t *testing.T) {
 	require.Equal(t, 404, code)
 }
 
+const errResp = `
+{
+	"kind": "Status",
+	"apiVersion": "v1",
+	"metadata": {},
+	"status": "Failure",
+	"message": "missing variable query for result",
+	"code": 500
+}
+`
+
 func TestQueryClient(t *testing.T) {
+	respondWithError := false
+	srv := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		if respondWithError {
+			rw.WriteHeader(http.StatusInternalServerError)
+			rw.Write([]byte(errResp))
+		}
+		rw.WriteHeader(http.StatusOK)
+		rw.Write([]byte("{}"))
+	}))
+	defer srv.Close()
+
+	t.Run("converts kube error responses to errors", func(t *testing.T) {
+		respondWithError = true
+		defer func() {
+			respondWithError = false
+		}()
+
+		client := v0alpha1.NewQueryDataClient(srv.URL+"/api/ds/query", nil, nil)
+		code, _, err := client.QueryData(context.Background(), v0alpha1.QueryDataRequest{})
+
+		require.Error(t, err)
+		require.ErrorContains(t, err, "Failure(500): missing variable query for result")
+		require.Equal(t, 500, code)
+	})
+}
+
+func TestQueryClient_Integration(t *testing.T) {
 	t.Skip()
 
 	client := v0alpha1.NewQueryDataClient("http://localhost:3000/api/ds/query", nil,
