ae33ecabaac34c9ac36d341808e0e3bd8cb107fb
Andreas Christou <andreas.christou@grafana.com>
2024-11-05 12:03:20 +0000

    Amend comment location

--- backend/internal/endpointctx/endpointctx.go
+++ backend/internal/endpointctx/endpointctx.go
@@ -1,6 +1,6 @@
-// This package has been added to expose the EndpointCtxKey to allow the datasource_metrics_middleware to read it 
 package internal
 
 type EndpointCtxKeyType struct{}
 
+// This package has been added to expose the EndpointCtxKey to allow the datasource_metrics_middleware to read it
 var EndpointCtxKey = EndpointCtxKeyType{}
