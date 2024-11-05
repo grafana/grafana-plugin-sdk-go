4a0a231bd38d00d3bcac1cdcbe7bddbc2b9935bd
Ivana Huckova <30407135+ivanahuckova@users.noreply.github.com>
2024-11-05 15:56:11 +0100

    Update data/time_series_errors.go

--- data/time_series_errors.go
+++ data/time_series_errors.go
@@ -4,7 +4,7 @@ import "errors"
 
 var (
 	ErrorNullTimeValues = errors.New("unable to process the data to wide series because input has null time values, make sure all time values are not null")
-	ErrorSeriesUnsorted = errors.New("unable to process the data because it is not sorted in ascending order by time, please updated your query to sort the data by time and try again")
+	ErrorSeriesUnsorted = errors.New("unable to process the data because it is not sorted in ascending order by time, please updated your query to sort the data by time if possible")
 	ErrInputFieldsWithoutRows = errors.New("can not convert to long series, input fields have no rows")
 )
 
