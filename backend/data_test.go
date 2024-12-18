75f920d187c977a71494205d727f65a934c4398d
Isabella Siu <isabella.siu@grafana.com>
2024-12-18 14:45:33 -0500

    Add helper for batching by time range

--- backend/data_test.go
+++ backend/data_test.go
@@ -3,6 +3,7 @@ package backend
 import (
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
@@ -102,4 +103,41 @@ func TestQueryDataRequest(t *testing.T) {
 			require.Empty(t, req.Headers)
 		})
 	})
+}
+
+func TestBatchDataQueriesByTimeRange(t *testing.T) {
+	start := time.Date(2024, time.November, 29, 0, 42, 34, 0, time.UTC)
+	FiveMin := time.Date(2024, time.November, 29, 0, 47, 34, 0, time.UTC)
+	TenMin := time.Date(2024, time.November, 29, 0, 52, 34, 0, time.UTC)
+	loc := time.FixedZone("UTC+1", 1*60*60)
+	FiveMinDifferentZone := time.Date(2024, time.November, 29, 1, 47, 34, 0, loc)
+	testQueries := []DataQuery{
+		{
+			RefID:     "A",
+			TimeRange: TimeRange{From: start, To: FiveMin},
+		},
+		{
+			RefID:     "B",
+			TimeRange: TimeRange{From: start, To: TenMin},
+		},
+		{
+			RefID:     "C",
+			TimeRange: TimeRange{From: start, To: FiveMinDifferentZone},
+		},
+	}
+	result := BatchDataQueriesByTimeRange(testQueries)
+	require.Equal(t, 2, len(result))
+	var FiveMinQueries = result[0]
+	var TenMinQueries = result[1]
+	if len(result[0]) == 1 {
+		TenMinQueries = result[0]
+		FiveMinQueries = result[1]
+	}
+
+	require.Equal(t, 2, len(FiveMinQueries))
+	require.Equal(t, "A", FiveMinQueries[0].RefID)
+	require.Equal(t, "C", FiveMinQueries[1].RefID)
+
+	require.Equal(t, 1, len(TenMinQueries))
+	require.Equal(t, "B", TenMinQueries[0].RefID)
 }
