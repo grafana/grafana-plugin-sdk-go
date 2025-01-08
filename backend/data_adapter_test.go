c6e7ae519b53dd469947ffea6ede6fbdd39845d7
Will Browne <will.browne@grafana.com>
2025-01-08 11:10:49 +0000

    add more tests

--- backend/data_adapter_test.go
+++ backend/data_adapter_test.go
@@ -10,10 +10,13 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/grafana/grafana-plugin-sdk-go/experimental/status"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/genproto/googleapis/rpc/errdetails"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/metadata"
-	"google.golang.org/grpc/status"
+	grpcstatus "google.golang.org/grpc/status"
 
 	"github.com/grafana/grafana-plugin-sdk-go/backend/httpclient"
 	"github.com/grafana/grafana-plugin-sdk-go/genproto/pluginv2"
@@ -164,7 +167,7 @@ func TestQueryData(t *testing.T) {
 			})
 			require.Error(t, err)
 
-			st := status.Convert(err)
+			st := grpcstatus.Convert(err)
 			require.NotNil(t, st)
 			require.NotEmpty(t, st.Details())
 			for _, detail := range st.Details() {
@@ -189,7 +192,7 @@ func TestQueryData(t *testing.T) {
 			})
 			require.Error(t, err)
 
-			st := status.Convert(err)
+			st := grpcstatus.Convert(err)
 			require.NotNil(t, st)
 			require.NotEmpty(t, st.Details())
 			for _, detail := range st.Details() {
@@ -214,11 +217,151 @@ func TestQueryData(t *testing.T) {
 			})
 			require.Error(t, err)
 
-			st := status.Convert(err)
+			st := grpcstatus.Convert(err)
 			require.NotNil(t, st)
 			require.Empty(t, st.Details())
 		})
 	})
+}
+
+func TestErrorSourceFromGrpcStatusError(t *testing.T) {
+	type args struct {
+		ctx func() context.Context
+		err func() error
+	}
+	type expected struct {
+		src   status.Source
+		found bool
+	}
+	tests := []struct {
+		name     string
+		args     args
+		expected expected
+	}{
+		{
+			name: "When error is nil",
+			args: args{
+				ctx: backgroundCtx,
+				err: noErr,
+			},
+			expected: expected{
+				src:   status.DefaultSource,
+				found: false,
+			},
+		},
+		{
+			name: "When error is not a grpc status error",
+			args: args{
+				ctx: backgroundCtx,
+				err: func() error {
+					return errors.New("oh no")
+				},
+			},
+			expected: expected{
+				src:   status.DefaultSource,
+				found: false,
+			},
+		},
+		{
+			name: "When error is a grpc status error without error details",
+			args: args{
+				ctx: backgroundCtx,
+				err: func() error {
+					return grpcstatus.Error(codes.Unknown, "oh no")
+				},
+			},
+			expected: expected{
+				src:   status.DefaultSource,
+				found: false,
+			},
+		},
+		{
+			name: "When error is a grpc status error with error details",
+			args: args{
+				ctx: backgroundCtx,
+				err: func() error {
+					st := grpcstatus.New(codes.Unknown, "oh no")
+					st, _ = st.WithDetails(&errdetails.ErrorInfo{
+						Metadata: map[string]string{
+							errorSourceMetadataKey: status.SourcePlugin.String(),
+						},
+					})
+					return st.Err()
+				},
+			},
+			expected: expected{
+				src:   status.SourcePlugin,
+				found: true,
+			},
+		},
+		{
+			name: "When error is a grpc status error with error details, but context already has a source",
+			args: args{
+				ctx: func() context.Context {
+					ctx := status.InitSource(context.Background())
+					err := status.WithSource(ctx, status.SourceDownstream)
+					require.NoError(t, err)
+					return ctx
+				},
+				err: func() error {
+					st := grpcstatus.New(codes.Unknown, "oh no")
+					st, _ = st.WithDetails(&errdetails.ErrorInfo{
+						Metadata: map[string]string{
+							errorSourceMetadataKey: status.SourcePlugin.String(),
+						},
+					})
+					return st.Err()
+				},
+			},
+			expected: expected{
+				src:   status.SourcePlugin,
+				found: true,
+			},
+		},
+		{
+			name: "When error is a grpc status error with error details but no error source",
+			args: args{
+				ctx: backgroundCtx,
+				err: func() error {
+					st := grpcstatus.New(codes.Unknown, "oh no")
+					st, _ = st.WithDetails(&errdetails.ErrorInfo{
+						Metadata: map[string]string{},
+					})
+					return st.Err()
+				},
+			},
+			expected: expected{
+				src:   status.DefaultSource,
+				found: false,
+			},
+		},
+		{
+			name: "When error is a grpc status error with error details but error source is not a valid source",
+			args: args{
+				ctx: backgroundCtx,
+				err: func() error {
+					st := grpcstatus.New(codes.Unknown, "oh no")
+					st, _ = st.WithDetails(&errdetails.ErrorInfo{
+						Metadata: map[string]string{
+							errorSourceMetadataKey: "invalid",
+						},
+					})
+					return st.Err()
+				},
+			},
+			expected: expected{
+				src:   status.DefaultSource,
+				found: false,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			src, ok := ErrorSourceFromGrpcStatusError(tt.args.ctx(), tt.args.err())
+			assert.Equal(t, tt.expected.src, src)
+			assert.Equal(t, tt.expected.found, ok)
+		})
+	}
 }
 
 var finalRoundTripper = httpclient.RoundTripperFunc(func(req *http.Request) (*http.Response, error) {
@@ -228,3 +371,7 @@ var finalRoundTripper = httpclient.RoundTripperFunc(func(req *http.Request) (*ht
 		Body:       io.NopCloser(bytes.NewBufferString("")),
 	}, nil
 })
+
+var backgroundCtx = func() context.Context { return context.Background() }
+
+var noErr = func() error { return nil }
