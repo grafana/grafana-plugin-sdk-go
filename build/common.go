f932e55e2cf52dd0400a1257180da39779e9bfa8
Hugo Kiyodi Oshiro <hugo.oshiro@grafana.com>
2025-01-13 10:23:00 +0100

    Feat: Build manifest on every backend build (#1191)

--- build/common.go
+++ build/common.go
@@ -98,8 +98,12 @@ func buildBackend(cfg Config) error {
 		return err
 	}
 
-	// TODO: Change to sh.RunWithV once available.
-	return sh.RunWith(cfg.Env, "go", args...)
+	err = sh.RunWithV(cfg.Env, "go", args...)
+	if err != nil {
+		return err
+	}
+	b := Build{}
+	return b.GenerateManifestFile()
 }
 
 func getBuildBackendCmdInfo(cfg Config) (Config, []string, error) {
@@ -335,7 +339,7 @@ func (Build) Backend() error {
 // BuildAll builds production executables for all supported platforms.
 func BuildAll() { //revive:disable-line
 	b := Build{}
-	mg.Deps(b.Linux, b.Windows, b.Darwin, b.DarwinARM64, b.LinuxARM64, b.LinuxARM, b.GenerateManifestFile)
+	mg.Deps(b.Linux, b.Windows, b.Darwin, b.DarwinARM64, b.LinuxARM64, b.LinuxARM)
 }
 
 //go:embed tmpl/*
