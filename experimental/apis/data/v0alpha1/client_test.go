aef7827f1b5c64cf042b14438cc011853f51c775
Gábor Farkas <gabor.farkas@gmail.com>
2025-01-30 09:42:06 +0100

    experimental: adjust the query client interface, remove unused code

--- experimental/apis/data/v0alpha1/client_test.go
+++ experimental/apis/data/v0alpha1/client_test.go
@@ -1,101 +1,56 @@
 package v0alpha1_test
 
 import (
-	"context"
-	"encoding/json"
-	"fmt"
-	"net/http"
-	"net/http/httptest"
+	"errors"
 	"testing"
 
+	"github.com/grafana/grafana-plugin-sdk-go/backend"
 	"github.com/grafana/grafana-plugin-sdk-go/experimental/apis/data/v0alpha1"
 	"github.com/stretchr/testify/require"
 )
 
-func TestClientWithBadURL(t *testing.T) {
-	client := v0alpha1.NewQueryDataClient("http://localhostXYZ:998/api/ds/query", nil, nil)
-	code, _, err := client.QueryData(context.Background(), v0alpha1.QueryDataRequest{})
-	require.Error(t, err)
-	require.Equal(t, 404, code)
-}
+func TestNewErrorQDR(t *testing.T) {
+	q1 := v0alpha1.DataQuery{}
+	q1.RefID = "A"
 
-const errResp = `
-{
-	"kind": "Status",
-	"apiVersion": "v1",
-	"metadata": {},
-	"status": "Failure",
-	"message": "missing variable query for result",
-	"code": 500
-}
-`
+	q2 := v0alpha1.DataQuery{}
+	q2.RefID = "B"
 
-func TestQueryClient(t *testing.T) {
-	respondWithError := false
-	srv := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, _ *http.Request) {
-		if respondWithError {
-			rw.WriteHeader(http.StatusInternalServerError)
-			_, err := rw.Write([]byte(errResp))
-			require.NoError(t, err)
+	err := errors.New("test error")
+
+	t.Run("NewErrorQDR works with zero refIds", func(t *testing.T) {
+		req := v0alpha1.QueryDataRequest{
+			Queries: []v0alpha1.DataQuery{},
 		}
-		rw.WriteHeader(http.StatusOK)
-		_, err := rw.Write([]byte("{}"))
-		require.NoError(t, err)
-	}))
-	defer srv.Close()
 
-	t.Run("converts kube error responses to errors", func(t *testing.T) {
-		respondWithError = true
-		defer func() {
-			respondWithError = false
-		}()
-
-		client := v0alpha1.NewQueryDataClient(srv.URL+"/api/ds/query", nil, nil)
-		code, _, err := client.QueryData(context.Background(), v0alpha1.QueryDataRequest{})
-
-		require.Error(t, err)
-		require.ErrorContains(t, err, "Failure(500): missing variable query for result")
-		require.Equal(t, 500, code)
+		qdr := v0alpha1.NewErrorQDR(req, err)
+		require.Empty(t, qdr.Responses)
 	})
-}
 
-func TestQueryClient_Integration(t *testing.T) {
-	t.Skip()
+	t.Run("NewErrorQDR works with single refId", func(t *testing.T) {
+		req := v0alpha1.QueryDataRequest{
+			Queries: []v0alpha1.DataQuery{q1},
+		}
 
-	client := v0alpha1.NewQueryDataClient("http://localhost:3000/api/ds/query", nil,
-		map[string]string{
-			"Authorization": "Bearer XYZ",
-		})
-	body := `{
-		"from": "",
-		"to": "",
-		"queries": [
-			{
-				"refId": "X",
-				"scenarioId": "csv_content",
-				"datasource": {
-					"type": "grafana-testdata-datasource",
-					"uid": "PD8C576611E62080A"
-				},
-				"csvContent": "a,b,c\n1,hello,true",
-				"hide": false
-			}
-		]
-	}`
-	qdr := v0alpha1.QueryDataRequest{}
-	err := json.Unmarshal([]byte(body), &qdr)
-	require.NoError(t, err)
+		qdr := v0alpha1.NewErrorQDR(req, err)
+		require.Len(t, qdr.Responses, 1)
 
-	code, rsp, err := client.QueryData(context.Background(), qdr)
-	require.NoError(t, err)
-	require.Equal(t, http.StatusOK, code)
+		require.ErrorIs(t, qdr.Responses["A"].Error, err)
+		require.Equal(t, qdr.Responses["A"].Status, backend.StatusBadRequest)
+	})
 
-	r, ok := rsp.Responses["X"]
-	require.True(t, ok)
+	t.Run("NewErrorQDR works with multiple refIds", func(t *testing.T) {
+		req := v0alpha1.QueryDataRequest{
+			Queries: []v0alpha1.DataQuery{q1, q2},
+		}
 
-	for _, frame := range r.Frames {
-		txt, err := frame.StringTable(20, 10)
-		require.NoError(t, err)
-		fmt.Printf("%s\n", txt)
-	}
+		qdr := v0alpha1.NewErrorQDR(req, err)
+		require.Len(t, qdr.Responses, 2)
+
+		require.ErrorIs(t, qdr.Responses["A"].Error, err)
+		require.Equal(t, qdr.Responses["A"].Status, backend.StatusBadRequest)
+
+		require.ErrorIs(t, qdr.Responses["B"].Error, err)
+		require.Equal(t, qdr.Responses["B"].Status, backend.StatusBadRequest)
+	})
 }
