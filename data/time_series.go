0eb5bc9a994a27a8fa79136f49df63a020236f9e
ivanahuckova <ivana.huckova@gmail.com>
2025-02-28 14:51:13 +0100

    Export ErrorInputFieldsWithoutRowsLongSeries error

--- data/time_series.go
+++ data/time_series.go
@@ -218,7 +218,7 @@ func LongToWide(longFrame *Frame, fillMissing *FillMissing) (*Frame, error) {
 		return nil, err
 	}
 	if longLen == 0 {
-		return nil, fmt.Errorf("can not convert to wide series, input fields have no rows")
+		return nil, ErrorInputFieldsWithoutRowsWideSeries
 	}
 
 	wideFrame := NewFrame(longFrame.Name, NewField(longFrame.Fields[tsSchema.TimeIndex].Name, nil, []time.Time{}))
@@ -420,7 +420,7 @@ func WideToLong(wideFrame *Frame) (*Frame, error) {
 	if err != nil {
 		return nil, err
 	} else if wideLen == 0 {
-		return nil, ErrorInputFieldsWithoutRows
+		return nil, ErrorInputFieldsWithoutRowsLongSeries
 	}
 
 	var uniqueValueNames []string                        // unique names of Fields that are value types
