a7f613991660903368ac5036cebd17ff4f3b53e6
Will Browne <will.browne@grafana.com>
2025-03-27 09:37:25 +0000

    lessen breaking changes

--- backend/config.go
+++ backend/config.go
@@ -7,6 +7,22 @@ import (
 	"github.com/grafana/grafana-plugin-sdk-go/config"
 )
 
+const (
+	AppURL                           = config.AppURL
+	ConcurrentQueryCount             = config.ConcurrentQueryCount
+	UserFacingDefaultError           = config.UserFacingDefaultError
+	SQLRowLimit                      = config.SQLRowLimit
+	SQLMaxOpenConnsDefault           = config.SQLMaxOpenConnsDefault
+	SQLMaxIdleConnsDefault           = config.SQLMaxIdleConnsDefault
+	SQLMaxConnLifetimeSecondsDefault = config.SQLMaxConnLifetimeSecondsDefault
+	ResponseLimit                    = config.ResponseLimit
+	AppClientSecret                  = config.AppClientSecret
+)
+
+func NewGrafanaConfig(m map[string]string) *config.GrafanaCfg {
+	return config.NewGrafanaCfg(m)
+}
+
 // GrafanaConfigFromContext returns Grafana config from context.
 func GrafanaConfigFromContext(ctx context.Context) *config.GrafanaCfg {
 	return config.GrafanaConfigFromContext(ctx)
